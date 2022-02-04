7dda32f44b26045da1d2c74d34f5b20956fd3937
cty3 <97644394+cty3@users.noreply.github.com>
2022-02-04 14:46:09 -0800

    codes that fail + lib

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,14 @@
-public class SkillDemo{}
+public class SkillDemo{
+    public static boolean isLeapYear(int x){
+        if(x<0){
+            throw new IllegalArgumentException("Please enter a year after BC");
+        }
+        if(x%4!=0){
+            return false;
+        }
+        if(x%100==0 && x%400!=0){
+            return false;
+        }
+        return true;
+    }
+}
